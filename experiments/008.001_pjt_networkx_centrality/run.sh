b2599c87e20d97d83e3cd72aabc350faaf3b5c1d
Paul Tomchik <pauljtomchik@gmail.com>
2025-04-26 17:16:01 -0400

    Cleaned up 008.001_pjt_networkx_centrality

--- experiments/008.001_pjt_networkx_centrality/run.sh
+++ experiments/008.001_pjt_networkx_centrality/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python -m tasks.batch_metrics_prefect_workflow \
+python -m xtasks.network_metrics_prefect_flow \
   --osm-pbf "../../data/processed/osm/nonservice-roadways-buffer-10mi-county-36001_nonservice-roadways-buffer-50mi-state-36_us-250101.osm.pbf"
